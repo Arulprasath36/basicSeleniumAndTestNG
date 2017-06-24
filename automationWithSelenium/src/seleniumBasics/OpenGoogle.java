5eca743612f78e3c898564597ba83b0b61a0b151
Arulprasath <arulprasath36@gmail.com>
2017-06-24 13:53:14 +0530

    Initial commit

---
+++ automationWithSelenium/src/seleniumBasics/OpenGoogle.java
@@ -0,0 +1,25 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OpenGoogle {
+
+public static void main(String[] args) throws InterruptedException {
+	
+	
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Elcot\\Desktop\\drivers\\gecko\\geckodriver.exe");  
+		WebDriver driver = new FirefoxDriver();
+		driver.navigate().to("http://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Bishop heber trichy");
+		driver.navigate().back();
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		driver.close();
+		driver.quit();
+}
+
+}
